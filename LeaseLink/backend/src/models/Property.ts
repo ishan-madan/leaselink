d2b5bbf71e5f463b319d038c34dc02fa138f76f7
Ishan Madan <im121@duke.edu>
2024-06-19 04:03:35 -0700

    moved vendor schema to a new file

--- LeaseLink/backend/src/models/Property.ts
+++ LeaseLink/backend/src/models/Property.ts
@@ -1,42 +1,5 @@
 import mongoose  from "mongoose";
-import { randomUUID } from "crypto";
-
-const vendorSchema = new mongoose.Schema({
-    vendorid: {
-        type: String,
-        default: randomUUID(),
-    },
-    vendorType: {
-        type: String,
-        required: true,
-    },
-    vendorName: {
-        type: String,
-        required: true,
-    },
-    vendorEmail: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return v || this.vendorPhone;
-            },
-            message: 'Either email or phone number is required.',
-        },
-    },
-    vendorPhone: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return v || this.vendorEmail;
-            },
-            message: 'Either phone number or email is required.',
-        },
-    },
-    vendorWebsite: {
-        type: String,
-        required: false,
-    },
-});
+import Vendor from "./Vendor.js";
 
 const propertySchema = new mongoose.Schema({
     address:{
@@ -44,7 +7,7 @@ const propertySchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    vendors: [vendorSchema],
+    vendors: [Vendor.schema],
 });
 
 export default mongoose.model("Property", propertySchema);
