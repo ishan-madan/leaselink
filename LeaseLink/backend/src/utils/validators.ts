7b3103fbfe747440e9f29d730d7b87c4c0ac3732
Ishan Madan <im121@duke.edu>
2024-06-19 04:01:40 -0700

    added validators for address, new vendor, delete vendor, and address in param

--- LeaseLink/backend/src/utils/validators.ts
+++ LeaseLink/backend/src/utils/validators.ts
@@ -1,5 +1,7 @@
-import { body, ValidationChain, validationResult } from "express-validator";
+import { body, param, ValidationChain, validationResult } from "express-validator";
 import {Request, Response, NextFunction} from "express";
+import validator from "validator";
+
 
 export const validate = async (validations:ValidationChain[]) => {
     return async(req:Request, res:Response, next:NextFunction) => {
@@ -38,4 +40,29 @@ export const chatCompletionValidator = [
 
 export const addressValidator = [
     body("address").notEmpty().withMessage("Address is required"),
+]
+
+export const vendorValidator: ValidationChain[] = [
+    body("vendorType").notEmpty().withMessage("Vendor Type is required").isString().withMessage("Vendor Type must be a string"),
+    body("vendorName").notEmpty().withMessage("Vendor Name is required").isString().withMessage("Vendor Name must be a string"),
+    body("vendorEmail").optional({ nullable: true, checkFalsy: true }).isEmail().withMessage("Invalid email format"),
+    body("vendorPhone").optional({ nullable: true, checkFalsy: true }).custom((value) => {
+        // Custom validation to check for valid phone number format
+        if (!validator.isMobilePhone(value, "any", { strictMode: false })) {
+            throw new Error("Invalid phone number format");
+        }
+        return true;
+    }),
+    body("vendorWebsite").optional({ nullable: true, checkFalsy: true }).isURL().withMessage("Invalid URL format"),
+    ...addressValidator,
+]
+
+export const vendorDeleteValidator: ValidationChain[] = [
+    param("address").notEmpty().withMessage("Address is required").isString().withMessage("Address must be a string"),
+    param("vendorType").notEmpty().withMessage("Vendor Type is required").isString().withMessage("Vendor Type must be a string"),
+    param("vendorName").notEmpty().withMessage("Vendor Name is required").isString().withMessage("Vendor Name must be a string"),
+]
+
+export const addressParamValidator: ValidationChain[] = [
+    param("address").notEmpty().withMessage("Address is required").isString().withMessage("Address must be a string"),
 ]
