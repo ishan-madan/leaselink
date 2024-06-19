3d8f7619e1291bdd21a3871075c70eea88da115c
Ishan Madan <im121@duke.edu>
2024-06-19 04:02:33 -0700

    added main backend route for vendor requests

--- LeaseLink/backend/src/routes/index.ts
+++ LeaseLink/backend/src/routes/index.ts
@@ -2,12 +2,14 @@ import {Router} from "express";
 import userRoutes from "./user-routes.js";
 import chatRoutes from "./chat-routes.js";
 import propertyRoutes from "./property-routes.js";
+import vendorRoutes from "./vendor-routes.js";
 
 const appRouter = Router();
 
 appRouter.use("/user", userRoutes) //domain/api/v1/user
 appRouter.use("/chat", chatRoutes) //domain/api/v1/chat
 appRouter.use("/property", propertyRoutes) //domain/api/v1/property
+appRouter.use("/vendor", vendorRoutes) //domain/api/v1/vendor
 
 
 export default appRouter;
