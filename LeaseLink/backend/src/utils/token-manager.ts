ee21eb961f942eed090646565e59d409c6c63527
Ishan Madan <im121@duke.edu>
2024-06-24 22:47:33 -0700

    added debug statement

--- LeaseLink/backend/src/utils/token-manager.ts
+++ LeaseLink/backend/src/utils/token-manager.ts
@@ -13,6 +13,7 @@ export const createToken = (id:string, email:string, expiresIn:string) => {
 
 export const verifyToken = async (req:Request, res:Response, next: NextFunction) => {
     const token = req.signedCookies[`${COOKIE_NAME}`];
+    console.log(token);
 
     if (!token || token.trim() === "") {
         return res.status(401).json({message:"Token not received"});
