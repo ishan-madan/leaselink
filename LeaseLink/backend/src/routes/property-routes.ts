3bea29b24456d33d17c3c4e1070414a262778890
Ishan Madan <im121@duke.edu>
2024-06-24 21:54:58 -0700

    added token and admin verifications to routes

--- LeaseLink/backend/src/routes/property-routes.ts
+++ LeaseLink/backend/src/routes/property-routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { addProperty, deleteProperty, getAllProperties } from "../controllers/property-controllers.js";
 import { addressValidator, validate } from "../utils/validators.js";
+import { verifyToken } from "../utils/token-manager.js";
+import { verifyAdmin } from "../controllers/vendor-controller.js";
 
 const propertyRoutes = Router();
 
@@ -8,9 +10,9 @@ const propertyRoutes = Router();
 propertyRoutes.get("/", getAllProperties);
 
 // send post request to database to add new property to database
-propertyRoutes.post("/add", await validate(addressValidator), addProperty);
+propertyRoutes.post("/add", await validate(addressValidator), verifyToken, verifyAdmin, addProperty);
 
 // send delete request to database to delete property
-propertyRoutes.delete("/delete/:address", deleteProperty);
+propertyRoutes.delete("/delete/:address", verifyToken, verifyAdmin, deleteProperty);
 
 export default propertyRoutes;
