beb0a063d47a32fed806e6c16411216b74a9fc2b
ishan-madan <ishan.madan2109@gmail.com>
2024-12-26 14:28:10 -0800

    fixed url again

--- LeaseLink/backend/src/app.ts
+++ LeaseLink/backend/src/app.ts
@@ -10,7 +10,7 @@ config();
 const app = express();
 
 // middlewares
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
+app.use(cors({origin:"https://leaselink-frontend.onrender.com", credentials:true}))
 
 app.use(express.json());
 
